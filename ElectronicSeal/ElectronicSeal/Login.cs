291eb075863f8c552ec2586a6f2ef6810b1ad507
inui <k-inui@sic-net.co.jp>
2021-11-28 18:15:39 +0900

    メール送信メソッド追加

--- ElectronicSeal/ElectronicSeal/Login.cs
+++ ElectronicSeal/ElectronicSeal/Login.cs
@@ -38,10 +38,14 @@ namespace ElectronicSeal
 			// 確認コードを生成する
 			Random rdm = new System.Random();
 			int kakuninCode = rdm.Next(100000, 999999);//6桁の乱数生成
-													   
 
+			//メール送信実行
+			string strToAddr = "skillinui@gmail.com"; //ToDo:送信先メールアドレス指定
+			string strSubject = "[電子印鑑アプリ]二段階認証用の確認コードのお知らせ"; //ToDo:タイトル指定
+			string strMessage="確認コード: "+ kakuninCode;
+			SendMail(strToAddr, strSubject, strMessage);
 
-			/**** ToDo相談　/
+			/**** ToDo相談　SMSサービス利用が有料だがSMS使うか？？/
 			//string connectionString = Environment.GetEnvironmentVariable("endpoint=https://resource01.communication.azure.com/;accesskey=YzFNe2boAchaW9NwSJC6qIXW/Tv3xrTKXl+0MYK6Hq2DiBPe51rSDULUDrMRy8WLgqqraOhaUdD0L1TZjloxtQ==");
 			string connectionString = "endpoint=https://resource01.communication.azure.com/;accesskey=YzFNe2boAchaW9NwSJC6qIXW/Tv3xrTKXl+0MYK6Hq2DiBPe51rSDULUDrMRy8WLgqqraOhaUdD0L1TZjloxtQ==";
 			//ToDo: Azure CommunicationServicesリソースから取得した接続文字列
@@ -54,12 +58,44 @@ namespace ElectronicSeal
 			);
 			//番号指定: https://knowledge.cpi.ad.jp/tech/131/
 			/****/
-			
+
 			Menu menu = new Menu();
 			menu.ShowDialog();
 		}
 
-
+		//メール送信用メソッド
+		//参考サイト: https://chiritsumo-blog.com/c-sharp-mail-send/#toc1
+		private void SendMail(string strToAddr, string strSubject, string strMessage)
+		{
+			using (var smtp = new MailKit.Net.Smtp.SmtpClient())
+			{
+				// メールサーバ接続情報 
+				string strHost = "sv150.xserver.jp"; //ToDo:メールサーバ指定
+				int nPort = 587;
+				MailKit.Security.SecureSocketOptions mailSecOpt = MailKit.Security.SecureSocketOptions.None;
+				///string strUsrId = "user";
+				///string strUsrPass = "password";
+				string strUsrAddr = "k-inui@sic-net.co.jp";// "user@chiritsumo-blog.com";
+				///Console.WriteLine(strToAddr + "にメール送信");
+				// SMTPサーバに接続
+				smtp.Connect(strHost, nPort, mailSecOpt);
+				// 認証が不要な場合は、以下をコメントアウト
+				//smtp.Authenticate(strUsrId, strUsrPass);
+				// 送信するメールを作成
+				MimeKit.MimeMessage mail = new MimeKit.MimeMessage();
+				MimeKit.BodyBuilder builder = new MimeKit.BodyBuilder();
+				mail.From.Add(new MimeKit.MailboxAddress("", strUsrAddr));
+				mail.To.Add(new MimeKit.MailboxAddress("", strToAddr));
+				mail.Subject = strSubject;
+				builder.TextBody = strMessage;
+				mail.Body = builder.ToMessageBody();
+				// メールを送信
+				smtp.Send(mail);
+				// SMTPサーバから切断
+				smtp.Disconnect(true);
+				///Console.WriteLine("メール送信完了");
+			}
+		}
 
 
 		private void btnGAccess_Click(object sender, EventArgs e)
