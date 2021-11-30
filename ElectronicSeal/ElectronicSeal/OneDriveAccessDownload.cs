4c2d5ef8d223035c6b3e5c338fa48afadba823ad
inui <k-inui@sic-net.co.jp>
2021-11-30 20:46:28 +0900

    [OneDrive]ダウンロードできた

--- ElectronicSeal/ElectronicSeal/OneDriveAccessDownload.cs
+++ ElectronicSeal/ElectronicSeal/OneDriveAccessDownload.cs
@@ -19,16 +19,11 @@ namespace ElectronicSeal
     public partial class OneDriveAccessDownload : Form
 	{
         public static IPublicClientApplication PublicClientApp;
-        
-        //private string ClientId = "(クライアントID)";//取得方法: https://www.ipentec.com/document/microsoft-azure-register-application-in-azure-active-directory
-        //private string TenantId = "(テナントID)";//取得方法: https://www.ipentec.com/document/microsoft-azure-register-application-in-azure-active-directory
-        
-        // TODO: Azure AD の Client Id をセット
+
+        //Azure AD の Client Id をセット　クライアントID取得方法: https://www.ipentec.com/document/microsoft-azure-register-application-in-azure-active-directory
         private const string ClientId = "b8e59a2d-719d-418b-8ec7-93fa5cea3955";
-        // TODO: Azure AD の Tenant Id をセット
+        //Azure AD の Tenant Id をセット　テナントID取得方法: https://www.ipentec.com/document/microsoft-azure-register-application-in-azure-active-directory
         private const string TenantId = "fb0c89cd-35b8-4479-97eb-a57d6fa0ccc6";
-        // TODO: Azure AD のアプリのシークレットをセット
-        //private const string ClientSecret = "Idq7Q~WgaCehKgjcQKowoS5QLcQ3m9R6Xmvzn";//-Mp7Q~c8u2Z7wXn8WDFJ.AfZCy7PZkHnd06c8
 
         public OneDriveAccessDownload()
 		{
@@ -66,16 +61,47 @@ namespace ElectronicSeal
               );
             GraphServiceClient client = new GraphServiceClient(prov);
 
-            System.IO.Stream stream = await client.Me.Drive.Items["(フォルダID)"].Content.Request().GetAsync();
-            byte[] buffer = new byte[stream.Length];
-            stream.Read(buffer, 0, buffer.Length);
-            stream.Close();
 
-            System.IO.FileStream fs = new System.IO.FileStream("test02_new.txt", System.IO.FileMode.CreateNew);
-            fs.Write(buffer, 0, buffer.Length);
-            fs.Close();
 
-            textBox1.Text += "ダウンロードしました。";
+            //ファイルID
+            string file_id = "";
+            //ファイル検索
+            IDriveSearchCollectionPage items = await client.Me.Drive.Search("test001.png").Request().GetAsync();//test02_new.txt
+
+            foreach (DriveItem item in items)
+            {
+                if (item.Folder == null)
+                {
+                    //textBox1.Text += string.Format("{0}\t{1}\r\n", item.Name, item.Id);
+                    //ファイルID取得
+                    file_id = item.Id;
+                }
+                else
+                {
+                    //textBox1.Text += string.Format("{0}\t{1}\tフォルダ\r\n", item.Name, item.Id);
+                    //ファイルID取得
+                    file_id = item.Id;
+                }
+            }
+
+            if (file_id != "")
+            {
+                System.IO.Stream stream = await client.Me.Drive.Items[file_id].Content.Request().GetAsync();
+                byte[] buffer = new byte[stream.Length];
+                stream.Read(buffer, 0, buffer.Length);
+                stream.Close();
+
+                //ダウンロード場所は「\bin\Debug以下指定」
+                System.IO.FileStream fs = new System.IO.FileStream("test001_downloaded.png", System.IO.FileMode.CreateNew);
+                fs.Write(buffer, 0, buffer.Length);
+                fs.Close();
+
+                textBox1.Text += "ダウンロードしました。";
+			}
+			else
+			{
+                textBox1.Text += "ファイルが見つかりませんでした。";
+            }
 
         }
 
